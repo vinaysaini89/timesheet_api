e53037dbb3cc4d72d2c28b0a8691ac93ce7b2e46
Shantanu singh <shantanu@mobileyug.com>
2017-04-14 10:59:20 +0530

    index file timezone

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin:*');
 header("Access-Control-Allow-Headers:*");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+date_default_timezone_set("Asia/Kolkata");
 /**
  * CodeIgniter
  *
